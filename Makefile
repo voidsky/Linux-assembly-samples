c88b55da55b9fe988457083aae220b4b603833a5
Voidsky <skuodis@gmail.com>
2018-12-19 00:22:25 +0200

    sumargs_syscall finished

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all:	hello_syscall hello
+all:	hello_syscall hello sumargs_syscall
 
 hello:	hello.o
 	gcc -no-pie -o hello hello.o
@@ -6,12 +6,21 @@ hello:	hello.o
 hello.o:
 	nasm -f elf64 -o hello.o hello.asm
 
+
 hello_syscall: hello_syscall.o
 	ld -o hello_syscall hello_syscall.o
 
 hello_syscall.o:
 	nasm -f elf64 -o hello_syscall.o hello_syscall.asm
 	
+sumargs_syscall: sumargs_syscall.o
+	ld -o sumargs_syscall sumargs_syscall.o
+
+sumargs_syscall.o:
+	nasm -f elf64 -o sumargs_syscall.o sumargs_syscall.asm
+
+
 clean:
 	-rm *.o $(objects) hello_syscall
 	-rm *.o $(objects) hello
+	-rm *.o $(objects) sumargs_syscall
--- sumargs_syscall.asm
+++ sumargs_syscall.asm
@@ -16,20 +16,24 @@ _start:
 	jne	argcError
 
 	add	rsp,8		; skip pointer to file name 
+	;mov	rax,SYS_WRITE
+	;mov	rdi,STD_IN
+	;pop	rsi
+	;mov	rdx,255
+	;syscall
 
-	;pop	rsi		; get first 'real' argument
-	;call	str_to_int
-	;mov	r10,rax
+	pop	rdx		; get first 'real' argument
+	call	str_to_int
+	mov	r10,rax
 
-	;pop	rsi		; get secind 'real' argument
-	;call	str_to_int
-	;mov	r11,rax
 
-	;add	r11,r10
+	pop	rdx		; get secind 'real' argument
+	call	str_to_int
+	mov	r11,rax
 
-	;mov	rax,r11
-	;xor	r12,r12
-	mov	rax,1234
+	add	r11,r10
+
+	mov	rax,r11
 	call	int_to_str
 
 	mov	rax,SYS_WRITE
@@ -53,21 +57,90 @@ exit:
 	mov	rdi,EXIT_CODE
 	syscall
 
+str_to_int:
+	mov	rbx,rdx
+	mov	rax,0
+	mov	rsi,0
+	mov	rcx,0
+	mov	rdx,0
+.leading_spaces:
+	cmp	[rbx],byte ' '
+	jne 	.next1
+	inc	rbx
+	jmp	.leading_spaces
+.next1:
+	cmp	[rbx], byte 0
+	jne	.next2
+	jmp	.end_parse
+.next2:
+	cmp	[rbx], byte '0'
+	jb	.not_numeric
+	cmp	[rbx], byte '9'
+	jbe	.next3
+.not_numeric:
+	jmp	.end_parse
+.next3:
+	mov	rdx,10
+	mul	edx
+	mov	dh,0
+	mov	dl,byte [rbx]
+	sub	dl,'0'
+	add	eax,edx
+	inc	rsi
+	inc	rbx
+	jmp	.next1
+.end_parse:
+	cmp	rsi,0
+	je	.error_return
+	clc
+	jmp	.return	
+.error_return:
+	stc
+	jmp	.return
+.return:
+	ret
+
+
+
 int_to_str:
-	
+	xor	rdi,rdi
+	xor	rbx,rbx
+.loop:	
 	mov	rdx,0
 	mov	rbx,10
 	div	rbx		;rax/rbx -> rax whole, rdx reminder
 
-	add	rdx,48		; convert reminder to ascii
+	add	dl,byte '0'		; convert reminder to ascii
+	
 	mov	byte [number+rdi],dl
 
 	inc	rdi
 
 	cmp	rax,0
-	jne	int_to_str
+	jne	.loop
 
 	mov	byte [number+rdi],0
+	
+	dec	rdi
+
+	xor	rcx,rcx
+	mov	rcx,rdi
+	mov	rdx,rdi
+	xor	rdi,rdi
+	
+.exchange:	
+	mov	al,byte [number+rcx]
+	mov	ah,byte [number+rdi]
+	mov	byte [number+rcx],ah
+	mov	byte [number+rdi],al
+	dec	rcx
+	cmp	rcx,rdi
+	je	.finished
+	inc	rdi
+	cmp	rcx,rdi
+	je	.finished
+	jmp	.exchange
+.finished:
 
 	ret
 
